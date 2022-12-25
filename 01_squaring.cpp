8d41b730d884cc90450002155c153e29638a07ae
ArtemShchuchev <shuaa@rambler.ru>
2022-12-25 11:47:14 +0700

    добавил гибкости

--- 01_squaring.cpp
+++ 01_squaring.cpp
@@ -1,7 +1,5 @@
 ﻿#include <iostream>
 #include <vector>
-#include <variant>
-#include <any>
 
 
 void printHeader();
@@ -12,23 +10,30 @@ void squaring(T & var)
 	var *= var;
 }
 
-template <>
-void squaring(std::vector<int> & vect)
+// не знаю насколько это правильно, но мне хотелось, чтоб
+// vector мог принимать разные типы, первый вариант был...
+// 
+// template <>
+// void squaring(std::vector<int>& vect)
+template <typename VType>
+void squaring(std::vector<VType>& vect)
 {
 	for (auto& data : vect) data *= data;
 }
 
-// перегружаю оператор << для cout (чтобы выводить std::vector<int>)
-std::ostream& operator<< (std::ostream& out, const std::vector<int>& vect)
+// перегружаю оператор << для cout (чтобы выводить std::vector<VType>)
+template <typename VType>
+std::ostream& operator<< (std::ostream& out, const std::vector<VType>& vect)
 {
 	for (const auto& val : vect) out << val << " ";
 	return out;
 }
 
-template <class Type>
-void prinToConsole(const std::string& str, const Type& data)
+template <typename dType>
+void prinToConsole(const std::string& str, const dType& data)
 {
 	std::cout << "[" << str << "]: " << data << std::endl;
+	if (str == "OUT") std::cout << std::endl;
 }
 
 int main()
@@ -40,10 +45,20 @@ int main()
 	squaring(intData);
 	prinToConsole("OUT", intData);
 
+	double doubleData(4.3);
+	prinToConsole("IN", doubleData);
+	squaring(doubleData);
+	prinToConsole("OUT", doubleData);
+
 	std::vector<int> vectorIntData{ -1, 4, 8 };
 	prinToConsole("IN", vectorIntData);
 	squaring(vectorIntData);
 	prinToConsole("OUT", vectorIntData);
+
+	std::vector<double> vectorDoubData{ -1, 4.2, 8.5 };
+	prinToConsole("IN", vectorDoubData);
+	squaring(vectorDoubData);
+	prinToConsole("OUT", vectorDoubData);
 
 	return 0;
 }
